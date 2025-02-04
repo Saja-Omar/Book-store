5b56edcb65468463a0787ac54d4098c7fb95dadd
saja obeidat <161194413+Saja-Omar@users.noreply.github.com>
2025-02-05 01:11:51 +0300

    Update About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,27 +1,15 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeBage from './pages/home/HomeBage';
-import About from './pages/about/About';
-import Authors from './pages/authors/Authors';
-import Cart from './pages/cart/Cart';
-import Book from './pages/book/Book';
-function App() {
-    return (
-        <BrowserRouter>
-            <Header />
-         <Routes>
-            <Route path="/" element={<HomeBage/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/athors" element={<Authors/>}/>
-            {/* <Route path="/cart" element={<Cart/>}/>   */}
-            <Route path="/book/:id" element={<Book/>}/>  
-           
-         </Routes>
-            <Footer/>
-        </BrowserRouter>
-    );
-}
+import "./about.css";
 
-export default App;
+const About = () => {
+  return (
+    <section className="about">
+      <h1>About Us</h1>
+      <p>We are an online book store</p>
+      <p>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </section>
+  );
+};
+
+export default About;
